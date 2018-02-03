7c6e88d4d5a2aa7c4d487a8ec5455d81db99aa22
Kacper Zelichowski <kafejek@gmail.com>
2018-02-03 16:28:55 +0100

    First and last commit

---
+++ src/main/java/com/codecool/tinkertailor/TinkerTailor.java
@@ -0,0 +1,40 @@
+package com.codecool.tinkertailor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TinkerTailor {
+
+
+    private List<Integer> tinkerTailor(int n, int k) {
+        List<Integer> numbers = addNumbers(n);
+        List<Integer> result = new ArrayList<Integer>();
+        int removed;
+        int startIndex = 0;
+
+        while (numbers.set() > 0) {
+            int numberToRemove = (startIndex + k - 1) % numbers.size();
+            removed = numbers.get(numberToRemove);
+            numbers.remove(numberToRemove);
+            result.add(removed);
+            startIndex = numberToRemove;
+        }
+
+        return result;
+    }
+
+    private List<Integer> addNumbers(int n) {
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        for (int i = 0; i <= n; i++) {
+            numbers.add(i);
+        }
+
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        TinkerTailor tt = new TinkerTailor();
+        System.out.println(tt.tinkerTailor(5, 3));
+    }
+}
